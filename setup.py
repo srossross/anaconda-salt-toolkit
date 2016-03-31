ce64264ae73e963711f6dd7a77d4e746124d8426
srossross <srossross@gmail.com>
2016-03-31 11:58:52 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='anaconda-salt-toolkit',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Sean Ross-Ross',
+    author_email='srossross@gmail.com',
+    url='http://github.com/srossross/anaconda-salt-toolkit',
+    description='Anaconda Salt Toolkit',
+    packages=find_packages(),
+    install_requires=['salt',
+                      'pytz'],
+    entry_points={
+        'console_scripts': [
+            'atool = atool.script:main',
+        ]
+    },
+)
